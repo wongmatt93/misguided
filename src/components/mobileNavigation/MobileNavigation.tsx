02a4081145e259cfd39c91c086f7a92fd1c3165b
Matthew <wongmatt93@gmail.com>
2023-02-19 16:14:56 -0500

    updated icons

--- src/components/mobileNavigation/MobileNavigation.tsx
+++ src/components/mobileNavigation/MobileNavigation.tsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
-import { AiFillHome } from "react-icons/ai";
-import { BsFillPlusSquareFill } from "react-icons/bs";
-import { ImMap } from "react-icons/im";
-import { RiUserFill } from "react-icons/ri";
-import { RiMessage2Fill } from "react-icons/ri";
+import {
+  RiDiscussFill,
+  RiSpyFill,
+  RiHome2Fill,
+  RiAddBoxFill,
+  RiLuggageCartFill,
+} from "react-icons/ri";
 import "./MobileNavigation.css";
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../../context/AuthContext";
@@ -28,29 +30,29 @@ const MobileNavigation = () => {
           <li>
             <NavLink to="/home">
               <div className="trip-icon">
-                <AiFillHome />
+                <RiHome2Fill />
                 {/* {tripRequests > 0 && <div className="notification-dot"></div>} */}
               </div>
             </NavLink>
           </li>
           <li>
             <NavLink to="/trips">
-              <ImMap />
+              <RiLuggageCartFill />
             </NavLink>
           </li>
           <li>
             <NavLink to="/plan-trip">
-              <BsFillPlusSquareFill />
+              <RiAddBoxFill />
             </NavLink>
           </li>
           <li>
             <NavLink to="/inbox">
-              <RiMessage2Fill />
+              <RiDiscussFill />
             </NavLink>
           </li>
           <li>
             <NavLink to="/user-profile">
-              <RiUserFill />
+              <RiSpyFill />
             </NavLink>
           </li>
         </ul>
