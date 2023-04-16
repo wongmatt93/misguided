3d43086197402482e89e7788521c3a48014d3101
Matthew <wongmatt93@gmail.com>
2023-04-15 22:23:49 -0400

    fixed up delete comment button

--- src/components/tripCommentsSection/CommentCard.tsx
+++ src/components/tripCommentsSection/CommentCard.tsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { RiMore2Line, RiDeleteBin5Fill } from "react-icons/ri";
 import AuthContext from "../../context/AuthContext";
 import useProfileFetcher from "../../hooks/useProfileFetcher";
 import useTimer from "../../hooks/useTimer";
@@ -16,10 +17,15 @@ interface Props {
 }
 
 const CommentCard = ({ trip, comment, refreshTrip }: Props) => {
-  const { refreshProfile } = useContext(AuthContext);
+  const { userProfile, refreshProfile } = useContext(AuthContext);
   const commentor: UserProfile | null = useProfileFetcher(comment.uid);
+  const [active, setActive] = useState(false);
   const timesUp = useTimer(600);
   const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    active && setActive(false);
+  };
 
   const handleDelete = async () => {
     await deleteCommentFromTrip(trip, comment);
@@ -31,23 +37,42 @@ const CommentCard = ({ trip, comment, refreshTrip }: Props) => {
     <>
       {commentor && timesUp && (
         <li className="CommentCard">
-          <img
-            className="commentor-image circle-image"
-            src={commentor.photoURL!}
-            alt={commentor.photoURL!}
-            onClick={() => navigate(`/profile/${commentor.uid}`)}
-          />
-          <div className="name-comment-container">
-            <h2 onClick={() => navigate(`/profile/${commentor.uid}`)}>
-              {commentor.username}
-            </h2>
-            <p className="comment">{comment.comment}</p>
-            <p className="date-time">
-              {new Date(Number(comment.date)).toLocaleString()}
-            </p>
+          <div
+            className="total-container"
+            style={{
+              left: active ? "-3.5em" : "0",
+            }}
+            onClick={handleCardClick}
+          >
+            <img
+              className="commentor-image circle-image"
+              src={commentor.photoURL!}
+              alt={commentor.photoURL!}
+              onClick={() => navigate(`/profile/${commentor.uid}`)}
+            />
+            <div className="name-comment-container">
+              <h2 onClick={() => navigate(`/profile/${commentor.uid}`)}>
+                {commentor.username}
+              </h2>
+              <p className="comment">{comment.comment}</p>
+              <p className="date-time">
+                {new Date(Number(comment.date)).toLocaleString()}
+              </p>
+            </div>
+            <RiMore2Line
+              style={{
+                display: userProfile?.uid === comment.uid ? "block" : "none",
+              }}
+              className="show-delete-button"
+              onClick={() => setActive(!active)}
+            />
           </div>
-          <Button type="button" onClick={handleDelete}>
-            delete
+          <Button
+            className="delete-button"
+            variant="danger"
+            onClick={handleDelete}
+          >
+            <RiDeleteBin5Fill />
           </Button>
         </li>
       )}
